08a294bb964368f8ab42f7274ff00aa5272ef341
Grzegorz Pandyra <gpandyra@gmail.com>
2020-07-06 20:20:04 +0000

    Alterations to match rebase with changes to UART

--- src/mcu1.c
+++ src/mcu1.c
@@ -1,53 +1,39 @@
-/*! @file main.c
-      @brief Main loop
-    
-    Starting point of software
+/*! @file mcu1.c
+    @brief Main loop
+    Starting point of software for MCU1
 */
 
-// #include <avr/io.h>
-// #include <util/delay.h>
-// #include "config.h"
-
 /* Local macro definitions */
-#define F_CPU 1000000L
-#define BAUD 2400
-#define MYUBRR F_CPU/16/BAUD-1
-#define FILE_ID "main.c"
+#define MCU1
 /* Local macro-like functions */
 /* Local static variables */
 /* Global variables */
 /* Local static functions */
 /* Global functions */
 
-// #define __AVR_ATmega8__
-
+#include "config.h"
 #include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "serial_interface.h"
-#include "serial_interrupts.h"
+#include "ISR.h"
+#include "iccm.h"
 
 /**
  * @brief Main function
  */ 
 int main(){
-    char data[] = "Hello from ATmega8";
+    char data[] = "Hello from MCU1";
     serial_init(F_CPU, BAUD);
     sei();
-    int i = 0;
     DDRB |= 0x01;
 
     while(1) /* Loop the messsage continously */
     { 
-        i = 0;
         serial_info(data);
-        _delay_ms(500);
-        PORTB &= 0x00;
-            i++;
-        _delay_ms(500);
-        PORTB |= 0x01;                          
-        // serial_read("qq");
+        _delay_ms(1000);
+        iccm_receive();      
     }
     return 0;
 }
