007992d6b3d386b222eb8cbb8a1af494c92467d0
Grzegorz Pandyra <gpandyra@gmail.com>
2020-08-01 10:59:16 +0000

    Added function serial_log_data(), common_const.h file, formatting changes

---
+++ include/common_const.h
@@ -0,0 +1,10 @@
+
+#ifndef COMMON_CONST_GUARD
+#define COMMON_CONST_GUARD
+
+#define NULL_CHAR '\0'   
+#define SPACE_CHAR ' '
+#define NEWLINE_CHAR '\n'
+#define COLON_CHAR ':'
+
+#endif /* COMMON_CONST_GUARD */
